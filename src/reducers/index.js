50f6c34babf997b027dad37908f2ce6b7714c492
mitchell budden <>
2018-07-22 14:13:53 -0400

    Renaming reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import user from './ReducerUser';
-import lists from './ReducerGoals';
+import lists from './ReducerLists';
 
 export default combineReducers({
     user,
