6c77bc25ba92390010871876bb149b1f365f93f1
Mitchell Budden <mitchbudden79@gmail.com>
2019-02-13 21:09:36 -0500

    Updating List Instructions component

--- src/components/Add/ListInstructions.jsx
+++ src/components/Add/ListInstructions.jsx
@@ -2,23 +2,47 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import "../Global.css";
 import "./ListInstructions.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { iconList } from "../../constants";
 
 class ListInstructions extends Component {
+    constructor() {
+        super();
+        this.instructions = [
+            {
+                instruction: "Enter your contact information",
+                icon: iconList[0].name
+            },
+            {
+                instruction:
+                    "Create a list of links to your favorite things - on Amazon if possible",
+                icon: iconList[1].name
+            },
+            {
+                instruction:
+                    "We review your list, and if it meets our guidelines, we put it on Blue Links",
+                icon: iconList[2].name
+            },
+            {
+                instruction:
+                    "When people like the items on your list and buy them, you get money for your recommendation!",
+                icon: iconList[3].name
+            }
+        ];
+    }
     render() {
         return (
             <div className="list-instructions-outer">
-                <h4>
-                    Enter your contact information and a list of links to your
-                    favorite things off Amazon
-                </h4>
-                <h4>
-                    We review your list, and if it meets our guidelines, we put
-                    it on Blue Links
-                </h4>
-                <h4>
-                    When people like the items on your list and buy them, you
-                    get money for your recommendation!
-                </h4>
+                {this.instructions.map((text, index) => {
+                    return (
+                        <div className="instructions-group" key={index}>
+                            <FontAwesomeIcon icon={text.icon} size="2x" />
+                            <h4 className="list-instruction">
+                                {text.instruction}
+                            </h4>
+                        </div>
+                    );
+                })}
             </div>
         );
     }
