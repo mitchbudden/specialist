4c1c39fe42d50cc139d2e1b4b63c34f3f449f7ff
Mitchell Budden <mitchbudden79@gmail.com>
2019-06-05 19:40:18 -0400

    Updating node packages and fixing security vulnerabilities

---
+++ build/precache-manifest.8ea23fe41e8385cfa758a867a3643d5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5b9f97cc4a39c917c4df8057d42965",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a0753cc22560582b381",
+    "url": "/static/css/main.557f86a0.chunk.css"
+  },
+  {
+    "revision": "d996ce8728b0113bb512",
+    "url": "/static/js/2.85a48fff.chunk.js"
+  },
+  {
+    "revision": "8a0753cc22560582b381",
+    "url": "/static/js/main.f1a02729.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a9e64254fd9d42175fbed5c03b918452",
+    "url": "/static/media/logo.a9e64254.jpg"
+  }
+]);
